5b1560c2e84ff3cbb3fab3b70e993c58d9171465
meng <gozhuimeng@outlook.com>
2025-05-04 00:26:15 +0800

    01.250504 定义数据存储结构

---
+++ dataFormat.h
@@ -0,0 +1,26 @@
+//
+// Created by zhuimeng on 2025/5/4.
+//
+
+#ifndef DATAFORMAT_H
+#define DATAFORMAT_H
+
+typedef struct {
+    char *str;
+    int len;
+    int size;
+}Node;
+
+typedef struct {
+    Node *data;
+    int len;
+    int size;
+}Row;
+
+typedef struct {
+    Row *data;
+    int len;
+    int size;
+}Col;
+
+#endif //DATAFORMAT_H
