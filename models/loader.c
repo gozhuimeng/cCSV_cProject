ec0c979e6ae89ecc06515c3d939a078d2a05fb37
meng <gozhuimeng@outlook.com>
2025-05-13 16:23:07 +0800

    05.250513 完成基本的存储部分的写函数(数据写入结构体部分)，可能需要进一步完善

--- models/loader.c
+++ models/loader.c
@@ -1,5 +1,6 @@
 //
 // Created by meng on 25-5-7.
+// DOC 数据加载
 //
 
 #include"loader.h"
@@ -8,7 +9,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INIT_SIZE 100
+#define INIT_NODE_SIZE 100
+#define INIT_ROW_SIZE 10
+#define INIT_COL_SIZE 10
 #define EXTEND_X 1.5
 
 
@@ -19,7 +22,7 @@
 Node *createNode() {
     Node *node = (Node *) malloc(sizeof(Node));
     node->len = 0;
-    node->size = INIT_SIZE;
+    node->size = INIT_NODE_SIZE;
     node->str = (char *) malloc(node->size * sizeof(char));
     return node;
 }
@@ -45,7 +48,7 @@ void extendNode(Node *node) {
 void clearData(Node *node) {
     node->len = 0;
     free(node->str);
-    node->size = INIT_SIZE;
+    node->size = INIT_NODE_SIZE;
     node->str = (char *) malloc(node->size * sizeof(char));
 }
 
@@ -95,4 +98,173 @@ void overwriteData(Node *node, char *data) {
         node->len = 0;
     }
     loadData(node, data);
+}
+
+/**
+ * 创建行
+ * @param node
+ * @return
+ */
+Row *createRow() {
+    Row *row = (Row *) malloc(sizeof(Row));
+    row->len = 0;
+    row->size = INIT_ROW_SIZE;
+    row->data = (Node **) malloc(row->size * sizeof(Node *));
+    return row;
+}
+
+/**
+ * 拓展行空间
+ * @param row
+ */
+void extendRow(Row *row) {
+    Node **temp = realloc(row->data, ((int) row->size * EXTEND_X) * sizeof(Node *));
+    if (temp == NULL) {
+        printf("行空间拓展失败\n");
+        return;
+    }
+    row->data = temp;
+    row->size = (int) row->size * EXTEND_X;
+}
+
+/**
+ * 清空行数据
+ * @param row
+ */
+void clearRow(Row *row) {
+    row->len = 0;
+    for (int i = 0; i < row->size; i++) {
+        freeNode(&row->data[i]);
+    }
+    free(row->data);
+    row->size = INIT_ROW_SIZE;
+    row->data = (Node **) malloc(row->size * sizeof(Node *));
+}
+
+/**
+ * 释放行
+ * @param row
+ */
+void freeRow(Row **row) {
+    if (*row == NULL) {
+        return;
+    }
+    for (int i = 0; i < (*row)->len; i++) {
+        free((*row)->data[i]->str);
+    }
+    free((*row)->data);
+    free(*row);
+}
+
+/**
+ * 添加行
+ * @param row
+ * @param node
+ */
+void appendRow(Row *row, Node *node) {
+    if (node == NULL) {
+        return;
+    }
+    if (row->len > row->size) {
+        extendRow(row);
+    }
+    row->data[row->len] = node;
+    row->len++;
+}
+
+/**
+ * 覆写行
+ * @param row
+ * @param node
+ */
+void overwriteRow(Row *row, Node *node) {
+    clearRow(row);
+    if (node == NULL) {
+        return;
+    }
+    appendRow(row, node);
+}
+
+/**
+ * 创建列
+ * @return
+ */
+Col *createCol() {
+    Col *col = (Col *) malloc(sizeof(Col));
+    col->len = 0;
+    col->size = INIT_COL_SIZE;
+    col->data = (Row **) malloc(col->size * sizeof(Row *));
+    return col;
+}
+
+/**
+ * 拓展列空间
+ * @param col
+ */
+void extendCol(Col *col) {
+    Row **temp = realloc(col->data, ((int) col->size * EXTEND_X) * sizeof(Row *));
+    if (temp == NULL) {
+        printf("列空间拓展失败\n");
+        return;
+    }
+    col->data = temp;
+    col->size = (int) col->size * EXTEND_X;
+}
+
+/**
+ * 清空列
+ * @param col
+ */
+void clearCol(Col *col) {
+    col->len = 0;
+    for (int i = 0; i < col->size; i++) {
+        freeRow(&col->data[i]);
+    }
+    free(col->data);
+    col->size = INIT_COL_SIZE;
+    col->data = (Row **) malloc(col->size * sizeof(Row *));
+}
+
+/**
+ * 释放列
+ * @param col
+ */
+void freeCol(Col **col) {
+    if (*col == NULL) {
+        return;
+    }
+    for (int i = 0; i < (*col)->len; i++) {
+        freeRow(&(*col)->data[i]);
+    }
+    free((*col)->data);
+    free(*col);
+}
+
+/**
+ * 添加列
+ * @param col
+ * @param row
+ */
+void appendCol(Col *col, Row *row) {
+    if (row == NULL) {
+        return;
+    }
+    if (col->len > col->size) {
+        extendCol(col);
+    }
+    col->data[col->len] = row;
+    col->len++;
+}
+
+/**
+ * 覆写列
+ * @param col
+ * @param row
+ */
+void overwriteCol(Col *col, Row *row) {
+    clearCol(col);
+    if (row == NULL) {
+        return;
+    }
+    appendCol(col, row);
 }
--- models/loader.h
+++ models/loader.h
@@ -18,4 +18,28 @@ void loadData(Node *node, char *data);
 
 void overwriteData(Node *node, char *data);
 
+Row *createRow();
+
+void extendRow(Row *row);
+
+void clearRow(Row *row);
+
+void freeRow(Row **row);
+
+void appendRow(Row *row, Node *node);
+
+void overwriteRow(Row *row, Node *node);
+
+Col *createCol();
+
+void extendCol(Col *col);
+
+void clearCol(Col *col);
+
+void freeCol(Col **col);
+
+void appendCol(Col *col, Row *row);
+
+void overwriteCol(Col *col, Row *row);
+
 #endif //LOADER_H
